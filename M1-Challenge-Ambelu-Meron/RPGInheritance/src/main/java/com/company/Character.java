0ad65fe1593ae9b4af0faadd89e4537585066f36
Meron Ambelu <meronambelu@Merons-MacBook-Air.local>
2022-07-01 11:05:09 -0400

    addition

--- M1-Challenge-Ambelu-Meron/RPGInheritance/src/main/java/com/company/Character.java
+++ M1-Challenge-Ambelu-Meron/RPGInheritance/src/main/java/com/company/Character.java
@@ -1,33 +1,15 @@
 package com.company;
 
 public class Character {
-    private String name;
 
-    private String abilities;
 
-    private boolean attackAnotherCharacter;
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    public String getAbilities() {
-        return abilities;
-    }
 
-    public void setAbilities(String abilities) {
-        this.abilities = abilities;
-    }
 
-    public boolean isAttackAnotherCharacter() {
-        return attackAnotherCharacter;
-    }
 
-    public void setAttackAnotherCharacter(boolean attackAnotherCharacter) {
-        this.attackAnotherCharacter = attackAnotherCharacter;
-    }
+
+
+
 }
